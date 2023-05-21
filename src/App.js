b45caf6c1e0d2da8ad65efb0236f2cf20b102bd9
MrBatata <mn.martinez@live.com.ar>
2023-05-21 04:30:48 -0300

    changes for v6.4

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
 import {
-  BrowserRouter,
-  NavLink,
-  Routes,
-  Route
-} from 'react-router-dom';
-/** Pages */
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
+
+// pages
 import Home from './pages/Home'
 import About from './pages/About'
-import { Link } from 'react-router-dom/dist';
 
-/** Main root */
+// layouts
+import RootLayout from './layouts/RootLayout'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+)
+
 function App() {
   return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>Jobarouter</h1>
-          {/* <Link to="/">Home</Link> */}
-          {/* <Link> does not have class=active when clicked */}
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="about">About</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
-          {/* Sames effect --> `path="/"` same as `index` */}
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   );
 }
 
